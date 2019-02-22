f1af0ecb02992c20886eac18ea2815ff0debd1c1
John Mitchell <jrmmba@outlook.com>
2019-02-21 19:53:22 -0800

    initial commit

---
+++ data.sql
@@ -0,0 +1,10 @@
+insert into todo (description, datestarted, completed)
+values ("Finish java-orders-swagger", datetime("2019-01-13 04:04:04"),  0),
+       ("Walk the dogs", datetime("2019-01-17 04:04:04"), 1),
+       ("Feed the turtles", datetime("2019-03-01 04:04:04"), 0),
+       ("Complete the sprint challenge", datetime("2019-02-22 04:04:04"), 0),
+       ("provide feedback to my instructor", datetime("2019-02-13 04:04:04"), 1);
+
+insert into users (username)
+values ("Bob"),
+       ("Jane");
