8c48305eebea166e48792e462d68b546eb1e216f
John Mitchell <jrmmba@outlook.com>
2019-05-09 12:51:04 -0700

    Create seeddata.java

---
+++ seeddata.java
@@ -0,0 +1,49 @@
+@Transactional
+@Component
+public class SeedData implements CommandLineRunner
+{
+    RoleRepository rolerepos;
+    UserRepository userrepos;
+    ToDoRepository todorepos;
+
+    public SeedData(RoleRepository rolerepos, UserRepository userrepos, ToDoRepository todorepos)
+    {
+        this.rolerepos = rolerepos;
+        this.userrepos = userrepos;
+        this.todorepos = todorepos
+    }
+
+    @Override
+    public void run(String[] args) throws Exception
+    {
+        Role r1 = new Role("admin");
+        Role r2 = new Role("user");
+
+        ArrayList<UserRoles> admins = new ArrayList<>();
+        admins.add(new UserRoles(new User(), r1));
+        admins.add(new UserRoles(new User(), r2));
+
+        ArrayList<UserRoles> users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+
+        rolerepos.save(r1);
+        rolerepos.save(r2);
+
+        User u1 = new User("barnbarn", "ILuvM4th!", users);
+        User u2 = new User("admin", "password", admins);
+        User u3 = new User("Bob", "password", users);
+        User u4 = new User("Jane", "password", users);
+
+        u4.getToDos().add(new ToDo("Finish java-orders-swagger", datetime("2019-01-13 04:04:04"), u4));
+        u4.getToDos().add(new ToDo("Feed the turtles", datetime("2019-03-01 04:04:04"), u4));
+        u4.getToDos().add(new ToDo("Complete the sprint challenge", datetime("2019-02-22 04:04:04"), u4));
+
+        u3.getToDos().add(new ToDo("Walk the dogs", datetime("2019-01-17 04:04:04"), u3),
+        u3.getToDos().add(new ToDo("provide feedback to my instructor", datetime("2019-02-13 04:04:04"), u3);
+
+        userrepos.save(u1);
+        userrepos.save(u2);
+        userrepos.save(u3);
+        userrepos.save(u4);
+    }
+}
