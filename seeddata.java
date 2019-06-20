b599b984abf638cdc038947e776472a3db1ff8ff
John Mitchell <jrmmba@outlook.com>
2019-06-20 12:19:30 -0700

    Update seeddata.java

--- seeddata.java
+++ seeddata.java
@@ -1,50 +1,50 @@
+// provided by Vivek Vishwanath
+
 @Transactional
 @Component
-public class SeedData implements CommandLineRunner
-{
+public class SeedData implements CommandLineRunner {
     RoleRepository rolerepos;
     UserRepository userrepos;
     ToDoRepository todorepos;
 
-    public SeedData(RoleRepository rolerepos, UserRepository userrepos, ToDoRepository todorepos)
-    {
+    public SeedData(RoleRepository rolerepos, UserRepository userrepos, ToDoRepository todorepos) {
         this.rolerepos = rolerepos;
         this.userrepos = userrepos;
-        this.todorepos = todorepos
+        this.todorepos = todorepos;
     }
 
     @Override
-    public void run(String[] args) throws Exception
-    {
+    public void run(String[] args) throws Exception {
         Role r1 = new Role("admin");
         Role r2 = new Role("user");
-
-        ArrayList<UserRoles> admins = new ArrayList<>();
-        admins.add(new UserRoles(new User(), r1));
-        admins.add(new UserRoles(new User(), r2));
-
-        ArrayList<UserRoles> users = new ArrayList<>();
-        users.add(new UserRoles(new User(), r2));
 
         rolerepos.save(r1);
         rolerepos.save(r2);
 
+        ArrayList<UserRoles> users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
         User u1 = new User("barnbarn", "ILuvM4th!", users);
-        User u2 = new User("admin", "password", admins);
-        User u3 = new User("Bob", "password", users);
-        User u4 = new User("Jane", "password", users);
-
-        // the date and time string should get coverted to a datetime Java data type. This is done in the constructor!
-        u4.getToDos().add(new ToDo("Finish java-orders-swagger", "2019-01-13 04:04:04", u4));
-        u4.getToDos().add(new ToDo("Feed the turtles", "2019-03-01 04:04:04", u4));
-        u4.getToDos().add(new ToDo("Complete the sprint challenge", "2019-02-22 04:04:04", u4));
-
-        u3.getToDos().add(new ToDo("Walk the dogs", "2019-01-17 04:04:04", u3));
-        u3.getToDos().add(new ToDo("provide feedback to my instructor", "2019-02-13 04:04:04", u3));
-
+        u1.getTodos().add(new Todo("Finish java-orders-swagger", new Date(), u1));
+        u1.getTodos().add(new Todo("Feed the turtles", new Date(), u1));
+        u1.getTodos().add(new Todo("Complete the sprint challenge", new Date(), u1));
         userrepos.save(u1);
+
+        ArrayList<UserRoles> admins = new ArrayList<>();
+        admins.add(new UserRoles(new User(), r1));
+        admins.add(new UserRoles(new User(), r2));
+        User u2 = new User("admin", "password", admins);
+        u2.getTodos().add(new Todo("Walk the dogs", new Date(), u2));
+        u2.getTodos().add(new Todo("provide feedback to my instructor", new Date(), u2));
         userrepos.save(u2);
+
+        users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u3 = new User("Bob", "password", users);
         userrepos.save(u3);
+
+        users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u4 = new User("Jane", "password", users);
         userrepos.save(u4);
     }
 }
