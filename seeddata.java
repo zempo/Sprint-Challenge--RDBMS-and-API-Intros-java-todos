248991a1e116580a2f17133dfeff20aed72bbaa2
John Mitchell <jrmmba@outlook.com>
2019-10-10 19:49:33 -0700

    Update seeddata.java

--- seeddata.java
+++ seeddata.java
@@ -1,50 +1,113 @@
-// provided by Vivek Vishwanath
+import com.lambdaschool.todo.models.*;
+import com.lambdaschool.todo.services.RoleService;
+import com.lambdaschool.todo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 @Transactional
 @Component
-public class SeedData implements CommandLineRunner {
-    RoleRepository rolerepos;
-    UserRepository userrepos;
-    ToDoRepository todorepos;
+public class SeedData implements CommandLineRunner
+{
+    @Autowired
+    RoleService roleService;
 
-    public SeedData(RoleRepository rolerepos, UserRepository userrepos, ToDoRepository todorepos) {
-        this.rolerepos = rolerepos;
-        this.userrepos = userrepos;
-        this.todorepos = todorepos;
-    }
+    @Autowired
+    UserService userService;
+
 
     @Override
-    public void run(String[] args) throws Exception {
+    public void run(String[] args) throws Exception
+    {
         Role r1 = new Role("admin");
         Role r2 = new Role("user");
+        Role r3 = new Role("data");
 
-        rolerepos.save(r1);
-        rolerepos.save(r2);
+        roleService.save(r1);
+        roleService.save(r2);
+        roleService.save(r3);
 
-        ArrayList<UserRoles> users = new ArrayList<>();
-        users.add(new UserRoles(new User(), r2));
-        User u1 = new User("barnbarn", "ILuvM4th!", users);
+        // admin, data, user
+        ArrayList<UserRoles> admins = new ArrayList<>();
+        admins.add(new UserRoles(new User(),
+                                 r1));
+        admins.add(new UserRoles(new User(),
+                                 r2));
+        admins.add(new UserRoles(new User(),
+                                 r3));
+        User u1 = new User("admin",
+                           "password",
+                           "admin@lambdaschool.local",
+                           admins);
+        u1.getUseremails()
+          .add(new Useremail(u1,
+                             "admin@email.local"));
+        u1.getUseremails()
+          .add(new Useremail(u1,
+                             "admin@mymail.local"));
         u1.getTodos().add(new Todo("Finish java-orders-swagger", new Date(), u1));
         u1.getTodos().add(new Todo("Feed the turtles", new Date(), u1));
         u1.getTodos().add(new Todo("Complete the sprint challenge", new Date(), u1));
-        userrepos.save(u1);
 
-        ArrayList<UserRoles> admins = new ArrayList<>();
-        admins.add(new UserRoles(new User(), r1));
-        admins.add(new UserRoles(new User(), r2));
-        User u2 = new User("admin", "password", admins);
+        userService.save(u1);
+
+        // data, user
+        ArrayList<UserRoles> datas = new ArrayList<>();
+        datas.add(new UserRoles(new User(),
+                                r3));
+        datas.add(new UserRoles(new User(),
+                                r2));
+        User u2 = new User("cinnamon",
+                           "1234567",
+                           "cinnamon@lambdaschool.local",
+                           datas);
+        u2.getUseremails()
+          .add(new Useremail(u2,
+                             "cinnamon@mymail.local"));
+        u2.getUseremails()
+          .add(new Useremail(u2,
+                             "hops@mymail.local"));
+        u2.getUseremails()
+          .add(new Useremail(u2,
+                             "bunny@email.local"));
         u2.getTodos().add(new Todo("Walk the dogs", new Date(), u2));
         u2.getTodos().add(new Todo("provide feedback to my instructor", new Date(), u2));
-        userrepos.save(u2);
+        userService.save(u2);
+
+        // user
+        ArrayList<UserRoles> users = new ArrayList<>();
+        users.add(new UserRoles(new User(),
+                                r2));
+        User u3 = new User("barnbarn",
+                           "ILuvM4th!",
+                           "barnbarn@lambdaschool.local",
+                           users);
+        u3.getUseremails()
+          .add(new Useremail(u3,
+                             "barnbarn@email.local"));
+        userService.save(u3);
 
         users = new ArrayList<>();
-        users.add(new UserRoles(new User(), r2));
-        User u3 = new User("Bob", "password", users);
-        userrepos.save(u3);
+        users.add(new UserRoles(new User(),
+                                r2));
+        User u4 = new User("puttat",
+                           "password",
+                           "puttat@school.lambda",
+                           users);
+        userService.save(u4);
 
         users = new ArrayList<>();
-        users.add(new UserRoles(new User(), r2));
-        User u4 = new User("Jane", "password", users);
-        userrepos.save(u4);
+        users.add(new UserRoles(new User(),
+                                r2));
+        User u5 = new User("misskitty",
+                           "password",
+                           "misskitty@school.lambda",
+                           users);
+        userService.save(u5);
     }
 }
